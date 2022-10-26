c337a43b5cd2edd82edf9a11a3968c52c4aa6fb6
imkarthi16 <karthiveera04@gmail.com>
2022-10-26 12:17:32 +0530

    working with ES6 Destructuring Objects

--- scaler-js-course/Module 5/destructuring.js
+++ scaler-js-course/Module 5/destructuring.js
@@ -4,11 +4,32 @@
 // particular variables to set a arrays
 
 
-// Destructuringn Arrays
+// Destructuring Arrays
 let arr = ["Hi", "I", "am", "Karthick"];
 
 // let a = arr[1] // I
 // let b = arr[2] // am
 
-let [a, b, c, d] = arr;
-console.log(c) 
+// let [a, b, c, d] = arr;
+// console.log(c) 
+
+
+// Destructureing Objects
+// Another name foer key using keys: <anothe_name> like name : n
+
+let myObj = {
+    name : 'Karthi',
+    age : 24,
+    gender : 'M',
+    address : {
+        country : 'India',
+        city : 'Chennai'
+    }
+}
+
+let {name : n, age: a, gender:g, address : {country:c} } = myObj
+
+console.log(n)// another name for key n 
+console.log(a)
+console.log(g)
+console.log(c) // Nested obj keys
