a51358b3a45d1b81fa2d44391cb3117243ee7e08
imkarthi16 <karthiveera04@gmail.com>
2022-10-26 11:56:36 +0530

    working with ES6 Destructuring

---
+++ scaler-js-course/Module 5/destructuring.js
@@ -0,0 +1,11 @@
+// The destructuring assignment syntax is a Javascript exprssion that makes it possible to unpack values
+// from arrays, or properties from objects, into distinct variables.
+
+// particular variables to set a arrays
+
+let arr = ["Hi", "I", "am", "Karthick"];
+
+// let a = arr[1] // I
+// let b = arr[2] // am
+
+let [a, b, c, d] = arr;
