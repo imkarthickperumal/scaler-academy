39db584edcc1dd4315cc15855e43a13042058d54
imkarthi16 <karthiveera04@gmail.com>
2022-10-26 12:38:10 +0530

    working with ES6 Modules and Modularity

---
+++ scaler-js-course/Module 5/calc.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  console.log(a + b);
+}
+function sub(a, b) {
+  console.log(a - b);
+}
+function mul(a, b) {
+  console.log(a * b);
+}
+function div(a, b) {
+  console.log(a / b);
+}
+
+module.exports = {
+  additon: add,
+  substraction: sub,
+  multification: mul,
+  division: div,
+};
---
+++ scaler-js-course/Module 5/main.js
@@ -0,0 +1,6 @@
+const calculator = require("./calc.js");
+
+calculator.additon(2, 4);
+calculator.substraction(4, 2);
+calculator.multification(2, 4);
+calculator.division(4, 2);
