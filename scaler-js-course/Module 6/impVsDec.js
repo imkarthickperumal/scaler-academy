828b9eccd058ed8c0e6b22b401340e2bb93b3c04
imkarthi16 <karthiveera04@gmail.com>
2022-11-01 09:46:58 +0530

    working with ES6 Functional Programming impVsDec

---
+++ scaler-js-course/Module 6/impVsDec.js
@@ -0,0 +1,19 @@
+// we will be given a number and we have to check that if the sqaure of that number is even or not
+
+// Imperative way of writing code
+
+// const a = 5;
+// const aSqaure = 5 * 5;
+// let isEven;
+
+// if (aSqaure % 2 == 0) {
+//   isEven = true;
+// } else {
+//   isEven = false;
+// }
+// console.log(isEven);
+
+// Declarative way of writing code
+
+const checkForSqaure = (x) => (x*x %2===0 ? true : flase)
+console.log(checkForSqaure(4))
