0603a9689dcb4d2f5dcd20540e2c10b1f8decf0a
imkarthi16 <karthiveera04@gmail.com>
2022-11-09 19:11:58 +0530

    working with ES6 OOPS Module Constructor

---
+++ scaler-js-course/Module 7/constructorFn.js
@@ -0,0 +1,16 @@
+function createCar(_name, _company, _color) {
+  this.name = _name;
+  this.company = _company;
+  this.color = _color;
+  this.drive = function () {
+    console.log(`I am driving ${this.name} and it is of ${this.color} color`);
+  };
+}
+
+let car1 = new createCar("X4", "BMW", "Black");
+let car2 = new createCar("S-class", "Merceds", "Red");
+
+console.log(car1);
+console.log(car2);
+
+car1.drive();
